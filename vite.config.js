e9ca4c956ae7909c89f65634e8362c432db83877
Jorge Urias <jurias@andrew.cmu.edu>
2026-02-21 17:42:36 -0500

    matching skin editor to link with bonkverse

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { BONKVERSE_BASE_URL } from "../config/env";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: BONKVERSE_BASE_URL,
         changeOrigin: true,
       },
     },
